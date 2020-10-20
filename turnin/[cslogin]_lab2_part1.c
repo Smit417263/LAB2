33c083641d7c7fce3f6a303458c2600631f3bb26
Smit417263 <56094552+Smit417263@users.noreply.github.com>
2020-10-20 15:01:59 -0700

    Update [cslogin]_lab2_part1.c

--- turnin/[cslogin]_lab2_part1.c
+++ turnin/[cslogin]_lab2_part1.c
@@ -1,7 +1,7 @@
 /*	Author: spate130
  *  Partner(s) Name: 
  *	Lab Section: 023
- *	Assignment: Lab #2  Exercise #3
+ *	Assignment: Lab #2  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
